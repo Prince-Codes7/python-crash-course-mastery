6f59083a279d19a3c345738eb76dcab55f28b206
Chibueze Princewill Onumadu <princewillonumadu7@gmail.com>
2026-03-27 16:02:10 +0100

    Chapter 2: Variables, Strings, Numbers, Comments + all exercises completed

---
+++ chapter_02/chapter_02_exercise.py
@@ -0,0 +1,14 @@
+# 2-1 Simple Message
+simple_message = "This is my first variable exercise!"
+print(simple_message)
+
+# 2-2 Simple Message
+simple_message = "First message"
+print(simple_message)
+
+simple_message = "Second message - I changed the variable!"
+print(simple_message)
+
+# 2-3 Personal Message
+name = "Prince-Code7"
+print("Hi " + name + " would you like to learn some python today?")
---
+++ chapter_02/variables.py
@@ -0,0 +1,13 @@
+# Chapter 2 - Variables and Simple Data Types
+message = "Hello Python World."
+print(message)
+
+message = "I am becoming a Python badass in 2026!"
+print(message)
+
+message = "Variables can change their value anytime!"
+print(mesage)
+
+author_name = "Eric Matthes"
+book_title = "Python Crash Course"
+print("I am reading " + book_title + " by " + author_name)
