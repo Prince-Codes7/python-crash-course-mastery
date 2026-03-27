f900dedcfe4c7f116d6311b04c5d7a249441afd6
Chibueze Princewill Onumadu <princewillonumadu7@gmail.com>
2026-03-27 14:45:04 +0100

    Chapter 1: Completed Hello World and all exercises

---
+++ chapter_01/infinite_skills.py
@@ -0,0 +1,3 @@
+print("If I keep practicing everyday I'll become a python badass!")
+print("I can already run code, debug errors and use Git.")
+print("By the end of this book I would have built a game, data visualization and a web app.")
